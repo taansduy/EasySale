ceb78e0d93c66990f1f0892be3e12a82ff6d0f2a
Taansduy <duyvt@ssg.vn>
2019-05-08 16:54:05 +0700

    add product row for search view

---
+++ app/src/main/java/com/example/asus/login_screen/ProductAdapter.java
@@ -0,0 +1,72 @@
+package com.example.asus.login_screen;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
+import com.example.asus.login_screen.Model.Product;
+
+import java.util.List;
+
+public class ProductAdapter extends RecyclerView.Adapter<ProductAdapter.ProductViewHolder> {
+    private Context mContext;
+    private List<Product> productList;
+
+    public ProductAdapter(Context mContext, List<Product> productList) {
+        this.mContext = mContext;
+        this.productList = productList;
+    }
+
+    @Override
+    public ProductViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
+        LayoutInflater inflater=LayoutInflater.from(mContext);
+        View view=inflater.inflate(R.layout.row_search_product,null);
+        ProductViewHolder holder= new ProductViewHolder(view);
+        return holder;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ProductViewHolder productViewHolder, int i) {
+        Product product=productList.get(i);
+        productViewHolder.tv_name.setText(product.getName());
+        productViewHolder.tv_price.setText((int) product.getSalePrice());
+        productViewHolder.tv_id.setText(product.getId());
+        productViewHolder.tv_quantity.setText(product.getQuantity());
+        if(product.getListImage().size()!=0)
+        {
+            Glide.with(mContext).load(product.getListImage().get(0)).into(productViewHolder.img);
+        }
+        else
+        {
+            productViewHolder.img.setImageResource(android.R.drawable.ic_menu_report_image);
+        }
+
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return productList.size();
+    }
+
+    class ProductViewHolder extends RecyclerView.ViewHolder{
+        ImageView img;
+        TextView tv_name,tv_price,tv_id,tv_quantity;
+
+        public ProductViewHolder(@NonNull View itemView) {
+            super(itemView);
+            img=itemView.findViewById(R.id.img_product);
+            tv_name=itemView.findViewById(R.id.tv_name);
+            tv_price=itemView.findViewById(R.id.tv_price);
+            tv_id=itemView.findViewById(R.id.tv_id);
+            tv_quantity=itemView.findViewById(R.id.tv_quantity);
+        }
+    }
+}
+
